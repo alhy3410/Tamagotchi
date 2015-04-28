d100c6513bf58f10ddd7856830a884823dc6f645
andrew & ian <student@epicodus.com>
2015-04-28 13:54:26 -0700

    pass for the second spec and will show that the tamagotchi is alive

--- lib/Tamagotchi.rb
+++ lib/Tamagotchi.rb
@@ -1,5 +1,34 @@
 class Tamagotchi
-  define_method(:initialize) do |name, food, activity, sleep|
+  define_method(:initialize) do |name, food, activity, rest|
+    @name = name
+    @food = food
+    @activity = activity
+    @rest = rest
+  end
 
+  define_method(:name) do
+    @name
+  end
+
+  define_method(:food_level) do
+    @food
+  end
+
+  define_method(:activity_level) do
+    @activity
+  end
+
+  define_method(:rest_level) do
+    @rest
+  end
+
+  define_method(:is_alive) do
+    binding.pry
+    if @food == 0 || @rest == 0 || @activity == 10
+    binding.pry
+      "Tamagotchi Passed Away"
+    else
+      "Tamagotchi is still alive"
+    end
   end
 end
--- spec/Tamagotchi_spec.rb
+++ spec/Tamagotchi_spec.rb
@@ -1,14 +1,22 @@
 require('rspec')
 require('Tamagotchi')
+require('pry')
 
 describe(Tamagotchi) do
   describe("#initialize") do
     it("will show the name and stats of the tamagotchi") do
-      my_pet = Tamagotchi.new("Fido")
+      my_pet = Tamagotchi.new("Fido",5,5,5)
       expect(my_pet.name()).to(eq("Fido"))
-      expect(my_pet.food_level()).to(eq(10))
-      expect(my_pet.activity_level()).to(eq(10))
-      expect(my_pet.sleep_level()).to(eq(10))
+      expect(my_pet.food_level()).to(eq(5))
+      expect(my_pet.activity_level()).to(eq(5))
+      expect(my_pet.rest_level()).to(eq(5))
+    end
+  end
+
+  describe("#is_alive") do
+    it("will check all the Tamagotchi pet's levels to see if alive") do
+      my_pet = Tamagotchi.new("Fido",5,5,5)
+      expect(my_pet.is_alive()).to(eq("Tamagotchi is still alive"))
     end
   end
 end
